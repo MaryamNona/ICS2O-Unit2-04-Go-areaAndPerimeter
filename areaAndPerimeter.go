83b46bb70dcd433ec8223e133c99bf70830c5410
MaryamNona <>
2021-05-19 18:44:07 +0000

    added code

---
+++ areaAndPerimeter.go
@@ -0,0 +1,27 @@
+// Created by: Maryam Nona
+// Created on: May 2021
+//
+// This program does basic math
+
+package main
+
+import "fmt"
+
+func main() {
+  // This function does addition
+  var length int
+  var width int
+
+  // input
+  fmt.Println("This program finds the area and perimeter of a rectangle")
+  fmt.Println()
+  fmt.Print("Enter the length: ")
+  fmt.Scanln(&length)
+  fmt.Println("Enter the width: ")
+  fmt.Scanln(&width)
+
+  // output
+  fmt.Println("The area is: ", length * width, "mm²")
+  fmt.Println("The perimeter is: ", (length + width) * 2, " mm")
+  fmt.Println("Done.")
+}
